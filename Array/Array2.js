7c0e5b49d279e0348b8f3ae3d75f78c610093fcd
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-09-02 11:32:52 +0530

    array

--- Array/Array2.js
+++ Array/Array2.js
@@ -1,6 +1,6 @@
-var arr = ["ram ", "shyam","gaurav","vishal","roshan","rohan"]
+// var arr = ["ram ", "shyam","gaurav","vishal","roshan","rohan"]
 
-var arr2 = ["royal","techno","soft"]
+// var arr2 = ["royal","techno","soft"]
 
 
 // var x = arr.concat(arr2)
@@ -8,3 +8,73 @@ var arr2 = ["royal","techno","soft"]
 
 // var x = arr.includes("vishal")
 // console.log(x);
+
+
+// var x = arr.join("-")
+// console.log(x);
+
+
+// var array = [12,34,56,76,34,23,78,98];
+
+
+// array.forEach((x)=>{
+//     console.log(x);
+    
+// })
+
+// for(let i=0;i<array.length;i++){
+//     console.log(array[i]);
+    
+// }
+
+var arr = ["ram ", "shyam","gaurav","vishal","roshan","rohan"]
+
+
+// arr.forEach((a)=>{
+//     console.log(a.toUpperCase());
+    
+// })
+
+
+var x = arr.filter((a)=>{
+    return a.length > 5
+})
+console.log(x);
+
+
+// Filter :- it will return new array with modified data 
+
+
+
+
+// arr.forEach((a)=>{
+    
+//     // a.length > 6 ? console.log(a) : console.log();
+//     console.log(a);
+    
+    
+    
+    
+// })
+
+
+
+
+
+// map :- it will return new array with same length
+// var x = arr.map((arr)=>{
+//     return arr.length > 4
+    
+// })
+// console.log(x);
+
+
+
+
+
+// for(let i=0;i<=arr.length;i++){
+//     console.log(arr[i].toUpperCase());
+    
+// }
+
+
