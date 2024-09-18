0a6a21baa238cab6a524f5036d0760c63f20fea9
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-09-18 11:42:50 +0530

    task

---
+++ Event/Event6.js
@@ -0,0 +1,36 @@
+// console.log(Math.floor(8.28));
+// console.log(Math.ceil(8.02));
+
+
+// console.log(Math.round(8.49));
+// console.log(Math.trunc(8.9873));
+
+
+// var x = Math.floor(Math.random() * 1000000)
+// console.log(x);
+
+
+
+
+    function changeClr(){
+
+        var txt = document.getElementById("txt").value 
+
+
+        if(txt === "square"){
+        var height = parseInt(prompt("Enter height"))
+        var width = parseInt(prompt("Enter width"))
+    
+        
+    
+    
+        var output = document.getElementById("output")
+        output.style.height = height + "px"
+        output.style.width = width + "px"
+        output.style.backgroundColor = "red"
+
+
+        }
+    
+    }
+
